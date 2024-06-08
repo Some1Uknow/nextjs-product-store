896bbdef271ba4c30dcdf66b7d4b5e1293184ece
Some1Uknow <raghu250407@gmail.com>
2024-06-08 18:26:31 +0530

    SSG fixed

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -1,10 +1,22 @@
-import ProductDetail from "@/components/ProductDetail";
+import ProductDetail from "../../../components/ProductDetail";
 
 async function fetchProduct(id) {
   const res = await fetch(`https://fakestoreapi.com/products/${id}`, {
-    cache: "force-cache", //SSG
+    cache: "force-cache",
   });
   return res.json();
+}
+
+export async function generateStaticParams() {
+  const res = await fetch("https://fakestoreapi.com/products", {
+    cache: "force-cache",
+  });
+  const products = await res.json();
+  const paths = products.map((product) => ({
+    id: product.id.toString(),
+  }));
+
+  return paths;
 }
 
 export default async function ProductDetailPage({ params }) {
--- app/products/page.jsx
+++ app/products/page.jsx
@@ -12,3 +12,6 @@ export default async function ProductsPage() {
 
   return <ProductList products={products} />;
 }
+
+
+
